1770b9734dcb30f27c52b04c8a9d199ffcdf6c62
Jin-devST <kpasd002@gmail.com>
2023-03-16 11:27:14 +0900

    상품 api 구현

--- pages/api/products/[id].ts
+++ pages/api/products/[id].ts
@@ -7,7 +7,7 @@ import productData from '../../../data/productMock.json'
 
 export default async function handler(
     request: NextApiRequest,
-    response: NextApiResponse<ProductData | null>
+    response: NextApiResponse<ProductData | undefined>
 ) {
     // 미들 웨어를 통하여 백엔드에서 데이터를 받아오거나, graphql 을 사용해서 데이터를 fetch 해온다.
     // 지금은 그냥 mock 데이터를 사용하여 가져옴
@@ -15,16 +15,15 @@ export default async function handler(
     // await middleWare.run()
     const {statusCode} = response
     const {query} = request
-    switch (statusCode) {
-        case HttpStatusCode.Ok :
-            response.status(statusCode).json(productData.find(product => product.id === parseInt(<string>query.id)) || null)
-            break;
-        case HttpStatusCode.BadRequest :
-            // response.status(statusCode).json(null) // 뭔가 에러 처리
-            break;
-        default:
-            break;
-        // default:
-        //     response.status(statusCode).json(productData.find(product => product.id === parseInt(<string>query.id)) || null)
-    }
+    // switch (statusCode) {
+    //     case HttpStatusCode.Ok :
+    //         break;
+    //     case HttpStatusCode.BadRequest :
+    //         // response.status(statusCode).json(null) // 뭔가 에러 처리
+    //         break;
+    //     default:
+    //         break;
+    // }
+    response.status(statusCode).json(statusCode === HttpStatusCode.Ok ? productData.find(product => product.id === parseInt(<string>query.id)) : undefined)
+
 }
