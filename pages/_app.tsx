564a4a0b4b2b20104591defa1d37c3b37d2424cf
Jin-devST <kpasd002@gmail.com>
2023-04-20 17:43:05 +0900

    [add] nextjs 에서 mobx 를 사용 하기 위한 기초 store 작업

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
-
+import {Provider} from 'mobx-react';
+import {useStore} from '../store/store'
 function MyApp({Component, pageProps}: AppProps) {
     /*
     *  최초 실행 파일, 이곳에서 렌더링 하는 값은 모든 페이지에 영향을 준다. ( 글로벌 이외에 앱에 부담이 될 만한 행동은 x)
     * */
+    const store = useStore(pageProps)
     return (
-        <Component {...pageProps} />
+        <Provider store={store}>
+            <Component {...pageProps} />
+        </Provider>
     )
 }
 
---
+++ store/store.js
@@ -0,0 +1,55 @@
+import {action, makeObservable} from "mobx";
+import {enableStaticRendering} from "mobx-react";
+import {useMemo} from "react";
+
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+enableStaticRendering(typeof window === "undefined");
+
+let store = null;
+
+const isServer = typeof window === "undefined";
+
+class Store {
+    constructor(props) {
+        makeObservable(this);
+        this.hydrate(props);
+    }
+    @action
+    hydrate = data => {
+        if (data) {
+            // this.userStore = new UserStore(data);
+        }
+    };
+}
+
+function initializeStore(initialData) {
+    // if _store is null or undifined return new Store
+    //otherwise use _store
+
+    // If your page has Next.js data fetching methods that use a Mobx store, it will
+    // get hydrated here, check `pages/ssg.js` and `pages/ssr.js` for more details
+
+
+    // For SSG and SSR always create a new store
+    // if (isServer) {
+    //     return {
+    //         cartStore: new CartStore(),
+    //         userStore: new UserStore(initialData),
+    //     };
+    // }
+    //
+    // // Create the store once in the client
+    // if (store === null) {
+    //     store = {
+    //         cartStore: new CartStore(),
+    //         userStore: new UserStore(initialData),
+    //     };
+    // }
+
+    return store;
+}
+
+export function useStore(initialState) {
+    return useMemo(() => initializeStore(initialState), [initialState]);
+}
